1e860b65cfb084a2ba4113a9b8c942a48c32ab12
MaksDim <demasig@yandex.ru>
2022-03-01 12:08:34 +0400

    first

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int coast = 5000;
+        int part = 20;
+        int bonus = coast / part;
+        System.out.println("Количество бонусных миль: " + bonus);
+    }
+}
